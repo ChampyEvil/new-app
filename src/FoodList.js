7ad0351be24973b0edfc024100719a88c55f9db8
Champ <champ@odds.team>
2024-07-05 16:11:02 +0700

    Add another menu

--- src/FoodList.js
+++ src/FoodList.js
@@ -6,6 +6,10 @@ import './App.css'; // เพิ่มการ import ไฟล์ CSS
 const FoodMenuList = () => {
   const [menu, setFastFoodMenu] = useState([]);
   const [noodle, setNoodleMenu] = useState([]);
+  const [fried, setFriedMenu] = useState([]);
+  const [friedRice, setFriedRiceMenu] = useState([]);
+  const [egg, setEggMenu] = useState([]);
+  const [withoutRice, setWithoutRiceMenu] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:8080/api/getFastFoodMenu')
@@ -27,6 +31,46 @@ const FoodMenuList = () => {
       });
   }, []);
 
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/getFriedMenu')
+      .then(response => {
+        setFriedMenu(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/getFriedRiceMenu')
+      .then(response => {
+        setFriedRiceMenu(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/getEggMenu')
+      .then(response => {
+        setEggMenu(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/getWithoutRiceMenu')
+      .then(response => {
+        setWithoutRiceMenu(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="food-menu-container">
       <h1>ร้าน ครัวแสนอร่อย</h1>
@@ -36,7 +80,7 @@ const FoodMenuList = () => {
       <table className="food-menu-table">
         <thead>
           <tr>
-            <th colSpan="2" className="section-header">Fast Food</th>
+            <th colSpan="2" className="section-header">Fast Food Menu</th>
           </tr>
           <tr>
             <th>Name</th>
@@ -57,7 +101,7 @@ const FoodMenuList = () => {
       <table className="food-menu-table">
         <thead>
           <tr>
-            <th colSpan="2" className="section-header">Noodle</th>
+            <th colSpan="2" className="section-header">Noodle Menu</th>
           </tr>
           <tr>
             <th>Name</th>
@@ -66,6 +110,90 @@ const FoodMenuList = () => {
         </thead>
         <tbody>
           {noodle.map(item => (
+            <tr key={item.name}>
+              <td>{item.name}</td>
+              <td>{item.price.toFixed(2)} Bath</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* ตารางสำหรับ Noodle */}
+      <table className="food-menu-table">
+        <thead>
+          <tr>
+            <th colSpan="2" className="section-header">Fried Menu</th>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fried.map(item => (
+            <tr key={item.name}>
+              <td>{item.name}</td>
+              <td>{item.price.toFixed(2)} Bath</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* ตารางสำหรับ Noodle */}
+      <table className="food-menu-table">
+        <thead>
+          <tr>
+            <th colSpan="2" className="section-header">Fried Rice Menu</th>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {friedRice.map(item => (
+            <tr key={item.name}>
+              <td>{item.name}</td>
+              <td>{item.price.toFixed(2)} Bath</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* ตารางสำหรับ Noodle */}
+      <table className="food-menu-table">
+        <thead>
+          <tr>
+            <th colSpan="2" className="section-header">Egg Menu</th>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {egg.map(item => (
+            <tr key={item.name}>
+              <td>{item.name}</td>
+              <td>{item.price.toFixed(2)} Bath</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* ตารางสำหรับ Noodle */}
+      <table className="food-menu-table">
+        <thead>
+          <tr>
+            <th colSpan="2" className="section-header">Without Rice Menu</th>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {withoutRice.map(item => (
             <tr key={item.name}>
               <td>{item.name}</td>
               <td>{item.price.toFixed(2)} Bath</td>
